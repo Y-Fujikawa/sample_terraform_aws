2053adcb77c54519829b7b4dd0051afc118b5842
Y-Fujikawa <fujikawa0@gmail.com>
2019-01-23 15:03:21 +0900

    Subnet周りを追加

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,27 @@
-resource "aws_vpc" "main" {
+# VPCの設定
+resource "aws_vpc" "sample_vpc" {
     cidr_block = "10.0.0.0/16"
     instance_tenancy = "default"
+
+    tags {
+        Name = "${format("sandbox-%02d", count.index + 1)}"
+    }
+}
+
+# Public Subnetの作成
+resource "aws_subnet" "public-a" {
+    vpc_id     = "${aws_vpc.sample_vpc.id}"
+    cidr_block = "10.0.1.0/24"
+
+    tags {
+        Name = "${format("sandbox-%02d", count.index + 1)}"
+    }
+}
+
+# Public Subnetの追加
+resource "aws_subnet" "public-c" {
+    vpc_id     = "${aws_vpc.sample_vpc.id}"
+    cidr_block = "10.0.2.0/24"
 
     tags {
         Name = "${format("sandbox-%02d", count.index + 1)}"
