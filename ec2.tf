15c7534cc39baf483c22cefe151a77a0e40c23a3
Y-Fujikawa <fujikawa0@gmail.com>
2019-01-23 19:02:43 +0900

    SSHのみ通信許可するようにした

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,12 @@
 resource "aws_instance" "sandbox" {
-    count = "${lookup(var.ec2_config, "count")}"
-    ami = "${lookup(var.ec2_config, "ami")}"
-    instance_type = "${lookup(var.ec2_config, "instance_type")}"
+    count                       = "${lookup(var.ec2_config, "count")}"
+    ami                         = "${lookup(var.ec2_config, "ami")}"
+    instance_type               = "${lookup(var.ec2_config, "instance_type")}"
+    vpc_security_group_ids      = ["${aws_security_group.sample_security_group.id}"]
+    subnet_id                   = "${aws_subnet.public-a.id}"
+    associate_public_ip_address = "true"
 
     tags {
-      Name = "${format("sandbox-%02d", count.index + 1)}"
+        Name = "${format("sandbox-%02d", count.index + 1)}"
     }
 }
--- security_group.tf
+++ security_group.tf
@@ -3,19 +3,26 @@ resource "aws_security_group" "sample_security_group" {
     description = "sample_security_group"
     vpc_id = "${aws_vpc.sample_vpc.id}"
 
+    # ingress {
+    #     from_port = 80
+    #     to_port = 80
+    #     protocol = "tcp"
+    #     cidr_blocks = ["0.0.0.0/0"]
+    # }
+
     ingress {
-        from_port = 80
-        to_port = 80
+        from_port = 22
+        to_port = 22
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
-    ingress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+    # ingress {
+    #     from_port = 443
+    #     to_port = 443
+    #     protocol = "tcp"
+    #     cidr_blocks = ["0.0.0.0/0"]
+    # }
 
     egress {
         from_port = 0
