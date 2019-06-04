5c5e90998a35f4f28569d06a46b55aae50264387
Y-Fujikawa <fujikawa0@gmail.com>
2019-06-04 12:36:17 +0900

    ECRのライフサイクルを追加

--- modules/ecr/ecr.tf
+++ modules/ecr/ecr.tf
@@ -1,3 +1,28 @@
 resource "aws_ecr_repository" "this" {
   name = "sample"
 }
+
+
+resource "aws_ecr_lifecycle_policy" "this" {
+  repository = "${aws_ecr_repository.this.name}"
+
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Expire images older than 7 days",
+      "selection": {
+        "tagStatus": "untagged",
+        "countType": "sinceImagePushed",
+        "countUnit": "days",
+        "countNumber": 7
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
